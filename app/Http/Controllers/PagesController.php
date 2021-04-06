f54f23b574d3b175c55ccb58e55c0b8b50cb2187
Ghie Cotcheza <ghie.cotcheza@gmail.com>
2021-04-06 10:05:35 +0200

    init

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        return view('index');
+
+
+    }
+
+    public function about(){
+        $names = ['Ghie', 'Harvey', 'Mike', 'Jessica'];
+
+        return view('about', [
+            'names'=>$names
+        ]);
+
+    }
+
+}
---
+++ resources/views/about.blade.php
@@ -0,0 +1,23 @@
+@for ($i = 0; $i <= 10; $i++)
+    <h2>{{ $i }}</h2>
+
+@endfor
+
+@foreach ($names as $name)
+    <h2> The name is {{ $name }}</h2>
+@endforeach
+
+
+@forelse ($names as $name )
+    <h2> Name is {{ $name }}</h2>
+
+@empty
+    <h2> There are no names</h2>
+@endforelse
+
+{{ $i = 0 }}
+@while ($i < 10)
+    <h2>{{ $i }}</h2>
+    {{ $i++ }}
+
+@endwhile
